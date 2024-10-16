e81dd54fe3ca24a17c1782cea64dc9d13368b456
diegobraz <diegobr73@hotmail.com>
2024-10-15 21:30:44 -0300

    feature: Create Todo Database

---
+++ app/src/main/java/com/example/todocompose/TodoRepositoryImpl.kt
@@ -0,0 +1,49 @@
+package com.example.todocompose
+
+import com.example.todocompose.data.TodoDao
+import com.example.todocompose.data.TodoEntity
+import com.example.todocompose.data.TodoRepository
+import com.example.todocompose.domain.Todo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class TodoRepositoryImpl(
+    private val dao: TodoDao
+) : TodoRepository {
+    override suspend fun insert(title: String, description: String?) {
+        val entity = TodoEntity(title = title, description = description, isComplete = false)
+        dao.insert(entity)
+    }
+
+    override suspend fun updateCompleted(id: Long, isCompleted: Boolean) {
+        val existingTodo = dao.getById(id) ?: return
+        val updatedTodo = existingTodo.copy(isComplete = isCompleted)
+        dao.insert(updatedTodo)
+    }
+
+    override suspend fun delete(id: Long) {
+        val existingTodo = dao.getById(id) ?: return
+        dao.delete(existingTodo)
+    }
+
+    override fun getAll(): Flow<List<Todo>> {
+        return dao.getAll().map { entities ->
+            entities.map { entity ->
+                Todo(
+                    id = entity.id,
+                    title = entity.title,
+                    description = entity.description
+                )
+            }
+        }
+    }
+    override suspend fun getBy(id: Long): Todo? {
+       return dao.getById(id)?.let { entity ->
+           Todo(
+               id = entity.id,
+               title = entity.title,
+               description = entity.description
+           )
+       }
+    }
+}
---
+++ app/src/main/java/com/example/todocompose/data/TodoRepository.kt
@@ -0,0 +1,17 @@
+package com.example.todocompose.data
+
+import com.example.todocompose.domain.Todo
+import kotlinx.coroutines.flow.Flow
+
+interface TodoRepository {
+
+    suspend fun insert(title: String, description: String?)
+
+    suspend fun updateCompleted(id: Long, isCompleted: Boolean)
+
+    suspend fun delete(id: Long)
+
+    fun getAll(): Flow<List<Todo>>
+
+    suspend fun getBy(id: Long): Todo?
+}
